6b20a3e80956604db054eeb3c06813e478e7dbd9
Vinícius Ribeiro <107771171+Vjfrib@users.noreply.github.com>
2023-01-12 04:42:51 +0000

    Tirei do console e coloquei na web page.
    Como fazer controles no Shell?

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello Express app!')
+  res.send('gott mit uns')
 });
 
 app.listen(3000, () => {
   console.log('server started');
-  console.log('gott mit uns');
+  
 });
